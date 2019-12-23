8382122f6e91f8c5d26d726558089736aafd7093
Callisto7 <gadzhievs69@gmail.com>
2019-12-23 03:54:17 +0300

    changed views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,13 @@
 class PostsController < ApplicationController
+
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
   def index
     @posts = Post.all
   end
 
   def show
-    @post = Post.find(params[:id])
+
   end
 
   def new
@@ -21,13 +24,30 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
+
+  end
+
+  def update
+    if @post.update_attributes(post_params)
+       redirect_to @post
+    else
+     render :edit
+    end
+  end
+
+  def destroy
+
+    @post.destroy
+    redirect_to post_path
   end
 
   private
 
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
   def post_params
     params.require(:post).permit(:title, :summary, :body)
   end
-
 end
