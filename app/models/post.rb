77ca035fc3e6facfcc43ff89bca9549356cf5d1d
Callisto7 <gadzhievs69@gmail.com>
2019-12-28 02:49:35 +0300

    adapted page

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,14 +6,4 @@ class Post < ApplicationRecord
 
   validates :title, :summary, :body, presence: true
 
-  def all_tags
-    self.tags.map(&:name).join(', ')
-  end
-
-  def all_tags=(names)
-    self.tags = names.split(', ').map do |name|
-      Tag.where(name: name.strip).first_or_create!
-    end
-  end
-
 end
