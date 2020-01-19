babd8214c6f981f971ae0023ab7c4ac2e27f8a55
Callisto7 <gadzhievs69@gmail.com>
2020-01-19 15:08:15 +0300

    changed registration fields

--- db/schema.rb
+++ db/schema.rb
@@ -16,24 +16,24 @@ ActiveRecord::Schema.define(version: 2019_12_29_093049) do
   enable_extension "plpgsql"
 
   create_table "pictures", force: :cascade do |t|
-    t.string "image"
+    t.string   "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.text "summary"
-    t.text "body"
+    t.string   "title"
+    t.text     "summary"
+    t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "image"
+    t.string   "image"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
+    t.string   "email", default: "", null: false
+    t.string   "encrypted_password", default: "", null: false
+    t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
